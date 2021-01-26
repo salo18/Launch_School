e528ef2eebe658a39295855a3afdd22c554f5e65
Salo M <salofb21410@gmail.com>
2021-01-26 20:37:09 +1300

    loops2

---
+++ RB101/lesson_4/Loops_Practice/Loops2/e2.rb
@@ -0,0 +1,9 @@
+# Modify the following code so that the loop stops if number is equal to or between
+# 0 and 10.
+
+loop do
+  number = rand(100)
+  puts number
+  break if number >= 0 && number <= 10
+end
+
