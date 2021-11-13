736f4c13952b0a0b32f06f5241abafcc1f753814
Salo M <salofb21410@gmail.com>
2021-11-12 17:00:30 -0800

    more practice

---
+++ RB120/129_Prep/collaborators.rb
@@ -0,0 +1,23 @@
+# class Dog
+#   def speak
+#     puts "woof"
+#   end
+# end
+
+# class Person
+#   attr_accessor :name, :pet
+
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+# bob = Person.new("Bob")
+# dog = Dog.new
+
+# bob.pet = dog
+
+# bob.pet.speak
+
+
+
---
+++ RB120/129_Prep/variable_scope.rb
@@ -0,0 +1,85 @@
+class Animal
+  def hi
+    puts "hey #{@name}"
+  end
+end
+
+class Dog < Animal
+  attr_reader :name
+  # def initialize
+  #   @name = "Dog"
+  # end
+
+  def bark
+    @name = "Dog"
+    # puts @name
+  end
+end
+
+class Lab < Dog
+
+end
+
+animal = Animal.new
+# p animal
+# animal.hi
+# animal.bark
+
+dog = Dog.new
+p dog
+dog.bark
+p dog
+
+lab = Lab.new
+p lab
+lab.bark
+p lab
+
+# Ginni reply:
+class Person
+  def intialize
+  end
+
+  def say_something
+    @speech = "Cognito Ero Sum"
+    puts @speech
+  end
+end
+
+class Child < Person
+end
+
+jr = Child.new
+p jr                    # <Child:0x000055604e8ce228>
+# note that there is no instance variable
+
+jr.say_something        # Cognito Ergo Sum
+p jr                    # #<Child:0x000055604e8ce228 @speech="Cognito Ero Sum">
+# now jr has an instance variable
+
+
+#
+class Person
+  def intialize
+  end
+
+  def say_something
+    @speech = "Cognito Ero Sum"
+    puts @speech
+  end
+end
+
+class Child < Person
+  def say_something
+    puts "goo goo gaa gaa"
+  end
+end
+
+jr = Child.new
+p jr                    # <Child:0x000055604e8ce228>
+# note that there is no instance variable
+
+jr.say_something        # goo goo gaa gaa
+p jr                    # #<Child:0x000055604e8ce228>
+# still no instance variable
+
