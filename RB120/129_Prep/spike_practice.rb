7cbc937702e20a622940aae79312a9b83e178c32
Salo M <salofb21410@gmail.com>
2021-11-29 13:39:32 -0800

    interview practice

--- RB120/129_Prep/spike_practice.rb
+++ RB120/129_Prep/spike_practice.rb
@@ -148,32 +148,190 @@ Preschool (by Natalie Thompson)
 # General dentists fill teeth
 =end
 
-module Pullable
-  def pull_teeth
-  end
-end
+# module Pullable
+#   def pull_teeth
+#   end
+# end
 
-class Dentist
-  def initialize
-    @graduated = true
-  end
-end
+# class Dentist
+#   def initialize
+#     @graduated = true
+#   end
+# end
 
-class OralSurgeons
-  include Pullable
+# class OralSurgeons
+#   include Pullable
 
-  def place_implants
-  end
-end
+#   def place_implants
+#   end
+# end
 
-class Orthodontists
-  def straighten_teeth
-  end
-end
+# class Orthodontists
+#   def straighten_teeth
+#   end
+# end
 
-class GeneralDentist
-  include Pullable
+# class GeneralDentist
+#   include Pullable
 
-  def fill_teeth
-  end
-end
+#   def fill_teeth
+#   end
+# end
+
+
+
+
+
+=begin
+# This exercise asks you to come up with a class design for a Role-playing Game.
+
+# The application has information about every player. They all have a name, health, strength and intelligence.
+
+# When each player is created, it gets:
+
+# a health value of 100
+# a random strength value (between 2 and 12, inclusive)
+# a random intelligence value (between 2 and 12 inclusive)
+# The random values are determined by a call to a #roll_dice method that cannot be accessed outside of the class.
+
+# You can set and change the strength and intelligence in the constructors. However, once an object is constructed, the values may not change.
+
+# Health can only be changed by the methods #heal and #hurt. Each method accepts one argument, the amount of change to the health. The #heal increases the health value by the indicated amount, while the #hurt decreases the value.
+
+# A player can be a warrior, a paladin, a magician, or a bard.
+
+# Warriors receive an additional 2 points of strength when they're created. The resulting strength range is thus between 4 and 14, inclusive.
+
+# Magicians receive an additional 2 points of intelligence when they're created. The resulting intelligence range is thus between 4 and 14, inclusive.
+
+# Warriors and paladins have the ability to wear armor. They need access to 2 additional methods: #attach_armor and #remove_armor.
+
+# Paladins, magicians and bards can cast spells. They need access to a #cast_spell method, that accepts one argument, spell.
+
+# Bards are a special type of magician that can also create potions. They have a #create_potion method.
+
+If you pass a player instance to #puts, it should print information about the player in this format:
+Name: John
+Class: Warrior
+Health: 100
+Strength: 7
+Inteligence: 5
+=end
+
+# module Armorable
+#   def attach_armor
+#   end
+
+#   def remove_armor
+#   end
+# end
+
+# module Spellable
+#   def cast_spell(spell)
+#   end
+# end
+
+# class Player
+#   attr_reader :name, :health, :strength, :intelligence
+#   attr_writer :health
+
+#   def initialize(name)
+#     @name = name
+#     @health = 100
+#     @strength = roll_dice(2, 12)
+#     @intelligence = roll_dice(2, 12)
+#   end
+
+#   def to_s
+#     puts "Name: #{name}"
+#     puts "Class: #{self.class}"
+#     puts "Health: #{health}"
+#     puts "Strength: #{strength}"
+#     puts "Intelligence: #{intelligence}"
+#   end
+
+#   def heal(change)
+#     self.health = health + change
+#   end
+
+#   def hurt(change)
+#     self.health = health - change
+#   end
+
+#   private
+#   def roll_dice(low, high)
+#     (low..high).to_a.sample
+#   end
+
+#   attr_writer :strength, :intelligence
+# end
+
+# class Warrior < Player
+#   include Armorable
+#   def initialize(name)
+#     super(name)
+#     @strength = roll_dice(4, 14)
+#   end
+# end
+
+# class Paladin < Player
+#   include Armorable
+#   include Spellable
+# end
+
+# class Magician < Player
+#   include Spellable
+#   def initialize(name)
+#     super(name)
+#     @intelligence = roll_dice(4, 14)
+#   end
+# end
+
+# class Bard < Player
+#   include Spellable
+
+#   def create_potion
+#   end
+# end
+
+# puts Warrior.new("Bob")
+# puts Paladin.new("Paddy")
+# puts Magician.new("Dave")
+# puts Bard.new("Simpson")
+
+
+
+# TEST NOTES:
+# need to fix puts on spike --- encoding and object are showing up
+
+
+=begin
+# Fish and birds are both types of animal
+# All animals can eat
+# Fish can swim, but not all animals can swim
+# Birds can fly, but not all animals can fly
+# Ducks are a type of bird that can swim, but some other birds cannot swim
+=end
+
+
+
+
+
+# come back to q 10 -- #== borrows functionality of the integer class == to compare the return values?
+
+# q 12 --- 3 ways to create getters??
+
+=begin
+You're designing a Pet Shelter App that tracks dogs and cats that are in the shelter, and have identified three classes
+that you need: PetShelter, Dog, and Cat. Each PetShelter has one or more Dogs and one or more Cats. In our application, the
+Dogs and Cats don't interact directly, and neither needs to know anything about the PetShelter to which they belong.
+
+Given the above details, what kind of Object Oriented relationship (inheritance, mixin, collaboration, or none) should
+exist between objects of each pair of classes, and why?
+
+PetShelter and Dog
+PetShelter and Cat
+Cat and Dog
+=end
+
+
