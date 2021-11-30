ed4164b181fb6bbc4f48a03cbc3c436016cfe31a
Salo M <salofb21410@gmail.com>
2021-11-29 21:04:26 -0800

    interview

--- RB120/129_Prep/interview_practice.rb
+++ RB120/129_Prep/interview_practice.rb
@@ -282,47 +282,90 @@ p gandolf.fireball #=> "casts fireball for 500 damage!"
 # ========================================================================
 
 # LS Man...legend says LS Man first appeared in SPOT.
+# module Flightable
+#   def fly
+#     "I am #{self.name}, I am a #{self.class.to_s.downcase} and I can ??"
+#   end
+# end
+
+# class Superhero
+#   include Flightable
+#   attr_accessor :ability
+#   attr_reader :name
+#   def self.fight_crime
+#     puts "I am #{self}!"
+#     Superhero.new("").announce_ability("coding skills")
+#   end
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def announce_ability(ability)
+#     # Ability.new -- does this have to come from Ability class?
+#     puts "I fight crime with my #{ability} ability!"
+#   end
+# end
+
+# class LSMan < Superhero; end
+
+# class Ability
+#   attr_reader :description
+
+#   def initialize(description)
+#     @description = description
+#   end
+# end
+
+# superman = Superhero.new('Superman')
+
+# p superman.fly # => I am Superman, I am a superhero, and I can fly!
+
+# LSMan.fight_crime
+# # => I am LSMan!
+# # => I fight crime with my coding skills ability!
+
+
 module Flightable
   def fly
-    "I am #{self.name}, I am a #{self.class.to_s.downcase} and I can ??"
+    fly = Ability.new("fly").description
+    puts "I am #{name}, I am a #{self.class.to_s.downcase} and I can #{fly}."
   end
 end
-
 class Superhero
   include Flightable
   attr_accessor :ability
-  attr_reader :name
+  attr_reader :name # I added this line... didn't see how to access name getter in Flightable without it
   def self.fight_crime
     puts "I am #{self}!"
-    Superhero.new("").announce_ability("coding skills")
+    Superhero.new("").announce_ability
   end
 
   def initialize(name)
     @name = name
   end
 
-  def announce_ability(ability)
-    # Ability.new -- does this have to come from Ability class?
-    puts "I fight crime with my #{ability} ability!"
+  def announce_ability
+    puts "I fight crime with my #{Ability.new("coding skills").description} ability!"
   end
 end
 
-class LSMan < Superhero; end
+class LSMan < Superhero ; end
 
 class Ability
   attr_reader :description
-
   def initialize(description)
     @description = description
   end
 end
 
 superman = Superhero.new('Superman')
-
-p superman.fly # => I am Superman, I am a superhero, and I can fly!
-
+superman.fly # => I am Superman, I am a superhero, and I can fly!
 LSMan.fight_crime
 # => I am LSMan!
 # => I fight crime with my coding skills ability!
 
+# Can only change 2 methods where you see #IMPLEMENTATION . I want you to use the Ability class. superman.fly and
+# LSMan.fight_crime  should produce the commented outputs below them and the words LSMan, Superman, superhero, fly and
+# coding skills should be interpolated.
 
