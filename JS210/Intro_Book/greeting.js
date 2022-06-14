ad2672813e6f077f6162fe744a55e44f03b82d18
Salo M <salofb21410@gmail.com>
2022-06-13 17:15:43 -0700

    input/output

---
+++ JS210/Intro_Book/greeting.js
@@ -0,0 +1,17 @@
+// let Sync = require('readline-sync');
+// let name = Sync.question("What's your name?\n");
+// console.log(`Good Morningm ${name}!`);
+
+
+// let rlSync = require('readline-sync');
+
+// let num1 = Number(rlSync.question('First number:'));
+// let num2 = Number(rlSync.question('Second number:'));
+// let sum = num1 + num2;
+
+// console.log(`The numbers ${num1} and ${num2} add up to ${sum}`);
+
+// let name = prompt("What's your name?");
+// console.log(`Good morning ${name}`);
+
+
