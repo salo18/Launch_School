164fd622edf62b0df9c8650b9733983413cde686
Salo M <69745872+salo18@users.noreply.github.com>
2022-08-09 10:24:35 -0700

    js215

---
+++ JS215/lesson1.js
@@ -0,0 +1,13 @@
+// iteration
+// function myForEach(array, func) {
+//   for (let i = 0; i < array.length; i += 1) {
+//     func(array[i], i, array);
+//   }
+// }
+
+// let min = Infinity;
+// let getMin = value => (min = value <= min ? value : min);
+// myForEach([4, 5, 12, 23, 3], getMin);
+// console.log(min);
+
+// filter
