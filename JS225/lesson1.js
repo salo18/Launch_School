5b50a9529161ea7369dfa29e2febf25da4fb184b
Salo M <69745872+salo18@users.noreply.github.com>
2022-09-22 12:09:23 -0700

    lesson 2

--- JS225/lesson1.js
+++ JS225/lesson1.js
@@ -51,4 +51,153 @@
 
 // console.log(hatchback.speed);
 
+
+
 // FACTORIES PRACTICE
+
+// let chile = {
+//   name: 'The Republic of Chile',
+//   continent: 'South America',
+//   getDescription() {
+//     return this.name + ' is located in ' + this.continent + '.';
+//   },
+// };
+
+// let canada = {
+//   name: 'Canada',
+//   continent: 'North America',
+//   getDescription() {
+//     return this.name + ' is located in ' + this.continent + '.';
+//   },
+// };
+
+// let southAfrica = {
+//   name: 'The Republic of South Africa',
+//   continent: 'Africa',
+//   getDescription() {
+//     return this.name + ' is located in ' + this.continent + '.';
+//   },
+// };
+
+// function makeCountry(name, continent, visited) {
+//   if (visited === undefined) {
+//     visited = false;
+//   }
+
+//   return {
+//     name,
+//     continent,
+//     visited: false,
+//     visitCountry() {
+//       this.visited = true;
+//     },
+//     getDescription() {
+//       if (this.visited) {
+//         return `${this.name} is located in ${this.continent}. I have visited.`;
+//       } else {
+//         return `${this.name} is located in ${this.continent}. I haven't visited.`;
+//       }
+//     },
+
+//   };
+// }
+
+// let chile = makeCountry('The Republic of Chile', 'South America');
+// let canada = makeCountry('Canada', 'North America');
+// let southAfrica = makeCountry('The Republic of South Africa', 'Africa');
+
+// console.log(chile.getDescription());       // "The Republic of Chile is located in South America."
+// console.log(chile.visited);
+
+// chile.visitCountry();
+// console.log(chile.visited);
+// console.log(chile.getDescription());
+// // console.log(canada.getDescription());      // "Canada is located in North America."
+// // console.log(southAfrica.getDescription()); // "The Republic of South Africa is located in Africa."
+
+
+
+// OBJECTS PRACTICE
+// let scissorsId = 0;
+// let scissorsName = 'Scissors';
+// let scissorsStock = 8;
+// let scissorsPrice = 10;
+
+// let drillId = 1;
+// let drillName = 'Cordless Drill';
+// let drillStock = 15;
+// let drillPrice = 45;
+
+// function makeProduct(id, prodName, stock, price) {
+//   return {
+//     id,
+//     prodName,
+//     stock,
+//     price,
+//   }
+// }
+
+// let scissors = makeProduct(0, 'Scissors', 8, 10);
+// console.log(scissors);
+// let drill = makeProduct(1, 'Cordless Drill', 15, 45);
+// console.log(drill);
+
+
+// let scissors = {
+//   id: 0,
+//   name: 'Scissors',
+//   stock: 8,
+//   price: 10,
+// };
+
+// let drill = {
+//   id: 1,
+//   name: 'Cordless Drill',
+//   stock: 15,
+//   price: 45,
+// };
+
+// function changePrice(product, price) {
+//   if (price >= 0) {
+//     product.price = price;
+//   } else {
+//     return 'that is a negative number... try again.'
+//   }
+// }
+// // console.log(changePrice(drill, -10));
+
+// function describeProduct(obj) {
+//   console.log(`Name: ${obj.name}`);
+//   console.log(`ID: ${obj.id}`);
+//   console.log(`Price: $${obj.price}`);
+//   console.log(`Stock: ${obj.stock}`);
+// }
+// describeProduct(scissors);
+
+function makeProduct(id, prodName, stock, price) {
+  return {
+    id,
+    prodName,
+    stock,
+    price,
+    changePrice(price) {
+      if (price >= 0) {
+        this.price = price;
+      } else {
+        return 'that is a negative number... try again.'
+      }
+    },
+    describeProduct() {
+      console.log(`Name: ${this.prodName}`);
+      console.log(`ID: ${this.id}`);
+      console.log(`Price: $${this.price}`);
+      console.log(`Stock: ${this.stock}`);
+    },
+  }
+}
+
+let scissors = makeProduct(0, 'Scissors', 10, 20);
+console.log(scissors.price);
+scissors.changePrice(2)
+console.log(scissors.price);
+scissors.describeProduct();
